4aba3c057e732ebb7fd28edf5c2f00addef7cc35
Youssef Elsadany <youssefhussien211@gmail.com>
2025-06-25 07:19:01 +0300

    onboarding

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const String splashScreen="/SplashScreen";
+  static const String onboardingScreen="/OnboardingScreen";
+  static const String registerScreen="/RegisterScreen";
+
+}
---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color balckk=Colors.black;
+  static const Color green=Color(0xff00454A);
+  static const Color yellow=Color(0xffEEF6CE);
+  static const Color blackLight=Color(0xff171A1D);
+}
