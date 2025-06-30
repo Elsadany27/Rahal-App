03523bb3cd4cd5aeb39048bc7d97851e21907c22
Youssef Elsadany <youssefhussien211@gmail.com>
2025-06-30 09:29:50 +0300

    api_register

--- lib/features/auth/presentation/view/view model/auth_cubit.dart
+++ lib/features/auth/presentation/view/view model/auth_cubit.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reservation/features/auth/data/datasource/auth_datasource.dart';
 import 'package:reservation/features/auth/presentation/view/view%20model/auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState>{
   AuthCubit():super(InitialState());
+  AuthDatasource _authDatasource=AuthDatasource();
   bool statee=true;
 
   changeState(){
@@ -13,6 +16,22 @@ class AuthCubit extends Cubit<AuthState>{
     else{
       statee =false;
       emit(VisibleState());
+    }
+  }
+
+  //register
+  signupMethodCubit({String? email,String? password,required BuildContext context})async{
+    String? messageRegister;
+    emit(IsloadingRegisterState());
+    try{
+      messageRegister =await _authDatasource.registerMethod(email: email,password: password);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("$messageRegister")));
+      emit(SuccessRegisterState(succesMessage: messageRegister));
+      print("تم");
+    }catch(e){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("$messageRegister")));
+      print(e);
+      emit(FailureRegisterState(errorMessage: e.toString()));
     }
   }
 }
--- lib/features/auth/presentation/view/view model/auth_state.dart
+++ lib/features/auth/presentation/view/view model/auth_state.dart
@@ -3,3 +3,15 @@ class InitialState extends AuthState{}
 
 class VisibleState extends AuthState{}
 class UnVisibleState extends AuthState{}
+
+//register
+class IsloadingRegisterState extends AuthState{}
+class SuccessRegisterState extends AuthState{
+  String? succesMessage;
+
+  SuccessRegisterState({required this.succesMessage});
+}
+class FailureRegisterState extends AuthState{
+  String? errorMessage;
+  FailureRegisterState({this.errorMessage});
+}
